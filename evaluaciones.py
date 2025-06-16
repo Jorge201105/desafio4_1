c3abeba501b1b1020c314c387c736e2563950533
Jorge201105 <jimunozacuna@gmail.com>
2025-06-16 07:09:16 +0000

    W

--- evaluaciones.py
+++ evaluaciones.py
@@ -1,7 +1,7 @@
 from pizza import Pizza
 
-print(Pizza.precio)
-print(Pizza.validador("salsa de tomate", ["salsa de tomate","salsa bbq"]))
+print(Pizza.precio)## Metodo estatico se logra resultado sin instanciar
+print(Pizza.validador("salsa de tomate", ["salsa de tomate","salsa bbq"])) ## se instancia para obtener resultados de metodo no estatico
 c=Pizza() ##Instanciando la clase 
 c.pedido()
 print(c.valida)
