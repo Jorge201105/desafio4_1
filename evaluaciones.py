7dd9e2a6b978f4050e11a67a1364c528ca5a7b6d
Jorge201105 <jimunozacuna@gmail.com>
2025-06-17 15:30:36 +0000

    W

--- evaluaciones.py
+++ evaluaciones.py
@@ -1,9 +1,12 @@
 from pizza import Pizza
 
-print(Pizza.precio)## Metodo estatico se logra resultado sin instanciar
-print(Pizza.validador("salsa de tomate", ["salsa de tomate","salsa bbq"])) ## se instancia para obtener resultados de metodo no estatico
-c=Pizza() ##Instanciando la clase 
-c.pedido()
-print(c.valida)
+
+##print(Pizza.precio)## Metodo estatico se logra resultado sin instanciar
+##print(Pizza.validador("salsa de tomate", ["salsa de tomate","salsa bbq"]))
+  ## se instancia para obtener resultados de metodo no estatico
+c=Pizza() ##Instanciando la clase , se solicitan los inredientes, 
+c.pedido() ## Llamo el resultado del if en def pedido el que da el print, no pude colocar como variable el self.proteico porque esta fuera de la clase y arrogaba error.
+
+print(f" La pizza es valida si el parametro siguiente es True y no valida si es False : {c.valida}")
 print(Pizza.valida) ## Haciendo print sin instanciar la clase, 
 
--- pizza.py
+++ pizza.py
@@ -1,28 +1,28 @@
 from ingrediente import Ingredientes
 
 
-class Pizza():
+class Pizza():## Creando clases para crear objetos tipo Pizza
     precio = 10000
     tamaño = "familiar"
 
     
 
-    @staticmethod
+    @staticmethod ## creando elementos para validar dos argumentos
     def validador(elemento:str, lista:list[str]):
         if elemento in lista:
-            print("El elemento es valido")
+            ### print("El elemento es valido")
             return True
         else:
             print(" El elemento no es valido")
             return False
         
-    def pedido(self):
+    def pedido(self):### método para reaizar pedido
         proteico = input("Ingrese ingrediente proteico : ")
         vegetal1 = input("Ingrese primer vegetal : ")
         vegetal2 = input("Ingrese segundo vegetal : ")
         masa = input("Ingrese tipo de masa : ")
 
-        # usando atributo de clase ingredente
+        # usando atributo de clase ingredente se valida cada uno de los datos ingresados
 
         v1 = Pizza.validador(proteico,Ingredientes.proteicos)
         v2 = Pizza.validador(vegetal1,Ingredientes.vegetales)
@@ -34,7 +34,7 @@ class Pizza():
         self.vegetal2 = vegetal2
         self.masa = masa
 
-        if v1 and v2 and v3 and v4:
+        if v1 and v2 and v3 and v4:  ### se asigna a un atributo el valor True o False 
             print(f"El pedido es {self.proteico} , {self.vegetal1} , {self.vegetal2} , {self.masa}")
             self.valida = True
         else:
